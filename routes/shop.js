7ebbf445e681203abe2d5c2871eab2f3e7d44430
romka <romul3003@gmail.com>
2025-10-02 09:03:55 +0300

    Add Pug engine template

--- routes/shop.js
+++ routes/shop.js
@@ -2,14 +2,14 @@ const path = require('path');
 
 const express = require('express');
 
-const rootDir = require('../util/path'); 
+const rootDir = require('../util/path');
+const adminData = require('./admin');
 
 const router = express.Router();
 
 router.get('/', (req, res, next) => {
-  // Serve the shop.html file when the root route is accessed
-  res.sendFile(path.join(rootDir, 'views', 'shop.html'));
+  const products = adminData.products;
+  res.render('shop', {prods: products, pageTitle: 'Shop', path: '/'});
 });
-
 
 module.exports = router;
