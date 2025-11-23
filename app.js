df98a280269b6c896bab24938b5c43bbe3c25e53
romka <romul3003@gmail.com>
2025-11-23 20:41:02 +0200

    101 adding a product modul

--- app.js
+++ app.js
@@ -2,31 +2,30 @@ const path = require('path');
 
 const express = require('express');
 const bodyParser = require('body-parser');
-const { engine } = require('express-handlebars');
+
+const errorController = require('./controllers/error');
 
 const app = express();
 
-app.engine('hbs', engine({
-  layoutsDir: 'views/layouts/',
-  defaultLayout: 'main-layout',
-  extname: '.hbs',
-}));
-app.set('view engine', 'hbs');
+app.set('view engine', 'ejs');
 app.set('views', 'views');
 
-const adminData = require('./routes/admin');
+const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
+
+app.use((req, res, next) => {
+  res.locals.path = req.originalUrl; // now `path` is available in EJS templates
+  next();
+});
 
 // Middleware to parse incoming request bodies
 app.use(bodyParser.urlencoded({ extended: false }));
 // Serve static files from the 'public' directory
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/admin', adminData.routes);
+app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
-app.use((req, res, next) => {
-  res.status(404).render('404', { pageTitle: 'Page Not Found' });
-});
+app.use(errorController.get404);
 
 app.listen(3000);
