3095aaef361e9f3c5dcd15d27eb429b156cc5411
romka <romul3003@gmail.com>
2025-12-24 22:33:52 +0200

    The Model View Controller (MVC)

--- models/product.js
+++ models/product.js
@@ -1,4 +1,17 @@
-const products = [];
+const fs = require("fs");
+const path = require("path");
+const rootDir = require("../util/path");
+
+const filePath = path.join(rootDir, "data", "products.json");
+
+const getProductsFromFile = (cb) => {
+  fs.readFile(filePath, (err, fileContent) => {
+    if (err) {
+      return cb([]);
+    }
+    cb(JSON.parse(fileContent));
+  });
+};
 
 module.exports = class Product {
   constructor(title) {
@@ -6,10 +19,16 @@ module.exports = class Product {
   }
 
   save() {
-    products.push(this);
+    getProductsFromFile((products) => {
+      products.push(this);
+
+      fs.writeFile(filePath, JSON.stringify(products), (err) => {
+        console.log(err);
+      });
+    });
   }
 
-  static fetchAll() {
-    return products;
+  static fetchAll(cb) {
+    getProductsFromFile(cb);
   }
 };
---
+++ prettier.config.cjs
@@ -0,0 +1,14 @@
+module.exports = {
+  tabWidth: 2,
+  useTabs: false,
+  semi: true,
+  singleQuote: true,
+  trailingComma: "es5",
+  tabWidth: 2,
+  printWidth: 100,
+  arrowParens: "always",
+  bracketSameLine: false,
+  bracketSpacing: true,
+  proseWrap: "always",
+  quoteProps: "as-needed",
+};
